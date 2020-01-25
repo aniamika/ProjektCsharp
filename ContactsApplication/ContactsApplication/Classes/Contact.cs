8b43e21a6b2e1cfcd389a29fa61e6be238234bb2
Anna Mika <anna.mika@avanade.com>
2020-01-25 19:13:13 +0100

    create a folder to all classes and define first class for single contact

---
+++ ContactsApplication/ContactsApplication/Classes/Contact.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApplication.Classes
+{
+    public class Contact
+    {
+        // SQlite attributes
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
